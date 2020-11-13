9c0a8128db3f7d97088563b1487ca7d5a6035c0f
Laura Roa <laualexandrar@gmail.com>
2020-11-12 18:51:41 -0800

    bubble + even numbers

--- src/BubbleRightToLeft.kt
+++ src/BubbleRightToLeft.kt
@@ -6,16 +6,17 @@ class BubbleRightToLeft {
 
 fun main() {
 
-    val myBubbleRightToLeft = arrayOf(78, 23, 64, 9, 2, 54, 2)
+    val myBubbleRightToLeft = arrayOf(56, 25, 64, 7, 89 ,0 , 56, 789, 0)
 
-    for (pass in 0 until (myBubbleRightToLeft.size - 1)) {
-        for (currentposition in 0 until (myBubbleRightToLeft.size - 1)) {
-            if (myBubbleRightToLeft[currentposition] < myBubbleRightToLeft[currentposition + 1]) {
-                val temp = myBubbleRightToLeft[currentposition]
-                myBubbleRightToLeft[currentposition] = myBubbleRightToLeft[currentposition + 1]
-                myBubbleRightToLeft[currentposition + 1] = temp
+    for(pass in 0 until (myBubbleRightToLeft.size-1)){
+        for (currentPosition in 0 until (myBubbleRightToLeft.size-1)){
+            if(myBubbleRightToLeft[currentPosition]< myBubbleRightToLeft[currentPosition +1]){
+               val temp = myBubbleRightToLeft[currentPosition]
+                myBubbleRightToLeft[currentPosition] = myBubbleRightToLeft[currentPosition+1]
+                myBubbleRightToLeft[currentPosition+1] = temp
+
             }
         }
     }
-    println("Sorted numbers: [%s]".format(myBubbleRightToLeft.joinToString(separator = ", ")))
+    println("Sorted numbers: [%s]".format(myBubbleRightToLeft.joinToString(separator= ", ")))
 }
--- src/EvenNumbersKtn.kt
+++ src/EvenNumbersKtn.kt
@@ -2,21 +2,78 @@ class EvenNumbersKtn {
 }
 
 fun main() {
-    val myListLau = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val myEvenNumbersKtn = arrayOf(5,33,64,3,2,2,5,66,5,9,11,3,2,22,5,44)
 
-    var i = myListLau.size - 1
+    var i = (myEvenNumbersKtn.size - 1)
+    var numb = 0
 
-        while (i >= 0) {
-            if (myListLau[i]%2 == 0){
-                if (i > 1){
-                    print(myListLau[i].toString() + ", ")
+    while (numb <= i){
+        if(myEvenNumbersKtn[numb] % 2 == 0){
 
-                } else {
-                    print(myListLau[i])
+            for (pass in 0 until (myEvenNumbersKtn.size-1)){
+                for (position in 0 until (myEvenNumbersKtn.size - pass - 1)) {
+                    if (myEvenNumbersKtn[position] > myEvenNumbersKtn[position+ 1]) {
+                        val temp = myEvenNumbersKtn[position]
+                        myEvenNumbersKtn[position] = myEvenNumbersKtn[position + 1]
+                        myEvenNumbersKtn[position + 1] = temp
+                    }
 
                 }
 
             }
-            i--
         }
+        numb ++
+    }
+    println("Sorted numbers: [%s]".format(myEvenNumbersKtn.joinToString(separator = ", ")))
 }
+
+//val myBubble = arrayOf(28, 18, 9, 67, 12, 43, 1, 23, 54)
+//
+//for (pass in 0 until (myBubble.size - 1)) {
+//    for (currentPosition in 0 until (myBubble.size - pass - 1)) {
+//        if (myBubble[currentPosition] > myBubble[currentPosition + 1]) {
+//            val temp = myBubble[currentPosition]
+//            myBubble[currentPosition] = myBubble[currentPosition + 1]
+//            myBubble[currentPosition + 1] = temp
+//        }
+//
+//    }
+//}
+//
+//println("Sorted numbers: [%s]".format(myBubble.joinToString(separator = ", ")))
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fun main() {
+//    val myListLau = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+//
+//    var i = myListLau.size - 1
+//
+//        while (i >= 0) {
+//            if (myListLau[i]%2 == 0){
+//                if (i > 1){
+//                    print(myListLau[i].toString() + ", ")
+//
+//                } else {
+//                    print(myListLau[i])
+//
+//                }
+//
+//            }
+//            i--
+//        }
+//}
